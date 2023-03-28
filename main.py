a972611ed4e04176d5b73cc12a0b2759141ebc12
Roman Tukhar <tukharr@gmail.com>
2023-03-28 13:00:40 +0300

    Add fucntion to merge same lines into one file

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+def same(file1, file2):
+    same_lines = []
+
+    for line in file1:
+        if line in file2:
+            same_lines.append(line)
+
+    with open("same.txt", 'w') as file:
+        file.write("".join(same_lines))
+
+
 def read_file(filename):
     with open(filename) as file:
         lines = file.readlines()
