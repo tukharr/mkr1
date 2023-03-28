830aa70a039253af86dd3aa8aa05b9d2c4c4abda
Roman Tukhar <tukharr@gmail.com>
2023-03-28 13:02:29 +0300

    Add fucntion to merge different lines into one file

--- main.py
+++ main.py
@@ -9,6 +9,22 @@ def same(file1, file2):
         file.write("".join(same_lines))
 
 
+def diff(file1, file2):
+    different_lines = []
+    all_lines = file2.copy()
+
+    for line in file1:
+        if line not in file2:
+            all_lines.append(line)
+
+    for line in all_lines:
+        if line not in file2 or line not in file1:
+            different_lines.append(line)
+
+    with open("diff.txt", 'w') as file:
+        file.write("".join(different_lines))
+
+
 def read_file(filename):
     with open(filename) as file:
         lines = file.readlines()
