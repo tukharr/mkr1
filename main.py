9e5017b9b4a02e88a413b1c28640f3d76b191134
Roman Tukhar <tukharr@gmail.com>
2023-03-28 13:46:20 +0300

    Fixed same and diff functions

--- main.py
+++ main.py
@@ -1,28 +1,37 @@
 def same(file1, file2):
+    file1_lines = read_file(file1)
+    file2_lines = read_file(file2)
     same_lines = []
 
-    for line in file1:
-        if line in file2:
+    for line in file1_lines:
+        if line in file2_lines:
             same_lines.append(line)
 
     with open("same.txt", 'w') as file:
         file.write("".join(same_lines))
 
+    return same_lines
+
 
 def diff(file1, file2):
-    different_lines = []
-    all_lines = file2.copy()
+    file1_lines = read_file(file1)
+    file2_lines = read_file(file2)
 
-    for line in file1:
-        if line not in file2:
+    different_lines = []
+    all_lines = file2_lines.copy()
+
+    for line in file1_lines:
+        if line not in file2_lines:
             all_lines.append(line)
 
     for line in all_lines:
-        if line not in file2 or line not in file1:
+        if line not in file2_lines or line not in file1_lines:
             different_lines.append(line)
 
     with open("diff.txt", 'w') as file:
         file.write("".join(different_lines))
+
+    return different_lines
 
 
 def read_file(filename):
